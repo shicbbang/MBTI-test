836092e7262f7026ae742f62e3a2cc962b167cb0
shigbbang <shigbbang1234@gmail.com>
2025-08-08 10:02:22 +0900

    0808 다시 작업 함

--- js/question.js
+++ js/question.js
@@ -1,15 +1,14 @@
 import { questions } from "./data.js";
 
-//console.log("질문 페이지");
+// console.log("질문페이지");
 
-//번호 출력
+// // 번호출력
+// console.log(questions[0].number)
+// console.log(questions[0].question)
+// console.log(questions[0].choices[0].text)
+// console.log(questions[0].choices[1].text)
 
-//console.log(questions[0].number)
-//console.log(questions[0].question)
-//console.log(questions[0].choices[0].text)
-//console.log(questions[0].choices[1].text)
-
-const progressBar = document.querySelector(".progress .value")
+const progressBar = document.querySelector(".progress .value");
 const numberEl = document.querySelector(".number");
 const questionEl = document.querySelector(".question");
 const choice1El = document.querySelector(".choice1");
@@ -17,49 +16,49 @@ const choice2El = document.querySelector(".choice2");
 
 let currentNumber = 0;
 
-//mbti 결과를 담을 변수를 선언
+// mbti 결과를 담을 변수 선언
 let mbti = '';
 
-//choice1El 과 choice2El에 클릭 이벤트를 걸어본다.
-// 콜백 함수 : 앞에서 정의한 조건이 만족 할 때 실행되는 함수
-
-choice1El.addEventListener('click', function () {
+// choice1El과 choiceEl2에 클릭 이벤트를 걸어놓는다.
+// 콜백함수 : 앞에서 정의한 조건이 만족될 때 실행되는 함수
+choice1El.addEventListener('click', function(){
   nextQuestion(0);
 })
-
-choice2El.addEventListener('click', function () {
+choice2El.addEventListener('click', function(){
   nextQuestion(1);
 })
 
-function nextQuestion(choiceNumber) {
+
+function nextQuestion(choiceNumber){
   if(currentNumber == questions.length - 1){
-   //검사한 mbti 결과를 갖고 결과 페이지로 이동
+    // 결과 페이지로 이동
+    // 검사 한 mbti 결과를 갖고
     console.log('결과 페이지로 이동할거야~ 구현 중...')
-    shoeResultPage();
+    showResultPage();
     return
   }
-  //mbti 결과 저장
+  // mbti 결과 저장
   mbti = mbti + questions[currentNumber].choices[choiceNumber].value
+  console.log("mbti = " + mbti)
   currentNumber = currentNumber + 1;
   console.log(currentNumber)
-  renderQuuestion();
+  renderQuestion();
 }
 
-function shoeResultPage(){
-  //쿼리 스트링을 만들어서 결과 페이지를 호출
+function showResultPage(){
+  // 쿼리 스트링을 만들어서 결과 페이지를 호출
   location.href = './result.html?mbti=' + mbti;
 }
 
 
-
-function renderQuuestion() {
+function renderQuestion(){
   const question = questions[currentNumber];
-
+  
   numberEl.innerHTML = question.number
   questionEl.innerHTML = question.question
   choice1El.innerHTML = question.choices[0].text
   choice2El.innerHTML = question.choices[1].text
-  progressBar.style.width = (currentNumber + 1) * 10 + '%';
+  progressBar.style.width = (currentNumber+1)*10 + '%';
 }
 
-renderQuuestion();
+renderQuestion();
--- js/result.js
+++ js/result.js
@@ -1,49 +1,46 @@
-import { results,mbtis } from "./data.js";
+import { results, mbtis } from "./data.js";
 
+// 1. title
 console.log(results[0].title)
+// 2. 혼자서 맨탈... 출력
 console.log(results[0].results[0])
+// 3. 창업가 출력
 console.log(results[0].jobs[1])
-//console.log(mbtis.intp);
-//const mbti = 'intp'
-console.log(mbtis['intp']); //이게 가장 나은 방법
+// 4. mbtis에서 intp 의 값을 출력하기
+console.log(mbtis.intp)
+// const mbti = 'intp'
 
-//http://127.0.0.1:5500/result.html?mbti=isfj
-//쿼리 스트링을 가져와야 함.
+
+// http://127.0.0.1:5500/result.html?mbti=infp
+// 쿼리스트링을 가져와야 함.
 const mbti = new URLSearchParams(location.search).get('mbti')
 console.log(mbtis[mbti])
+// 받아온 ?mbti=infp에 따른 results 결과를 받아서 변수로
+const resultMbti = results[mbtis[mbti]]
+console.log(resultMbti)
 
-// 1. 각각의 태그를 지정해야 함.
-//const titleEl = document querySelectorAll 
-//const characterEl = ~~~~ querySelectorAll
-//const boxEls = ~~~~~~~~~ querySelectorAll
-//const jobsEls = ~~~~~~~~~ querySelectorAll
-//const lectureEl = ~~~~~~~~~ querySelectorAll
-//const lectureImgEls = ~~~~~~~~~ querySelectorAll
-
+// 1. 각각의 태그을 지정해야 함.
+const titleEl = document.querySelector('.page-title')
+const characterEl =document.querySelector('.character')
+const boxEls = document.querySelectorAll('.box')
+const jobsEls = document.querySelectorAll('.job')
+const lectureEl = document.querySelector('.lecture')
+const lectureImgEl = document.querySelector('.lecture img')
 // 위의 내용에 innerHTML 로 표시
-// 단,  querySelectorAll 로 가져온 아이들은
-// fotEach 구문을 이용해서 처리(문법을 찾아서)
-//lecture 는 lecture.html 로 보이게 하면 됩니다
+// 단, querySelectorAll 로 가져온 아이들은
+// forEach 구문을 이용해서 처리(문법은 찾아서)
+// lecture 는 lecture.html페이지를 보이게 하면 됩니다.
 
-const titleEl = document.querySelectorAll('.title')
-const characterEl = document.querySelectorAll('.character')
-const boxEls =  document.querySelectorAll('.boxs')
-const jobsEls =  document.querySelectorAll('.jobs')
-const lectureEl =  document.querySelectorAll('.lecture')
-const lectureImgEls =  document.querySelectorAll('.lectureImg')
+titleEl.innerHTML = resultMbti.title
+characterEl.src = resultMbti.character
 
-titleEl.forEach((el, i) => {
-  el.innerHTML = 'titleEl {i + 1}';
-});
+boxEls.forEach(function(box, i){
+  box.innerHTML = resultMbti.results[i]
+})
 
-characterEl.forEach((el, i) => {
-  el.innerHTML = 'chracter {i + 1}';
-});
+jobsEls.forEach((job, i)=>{
+  job.innerHTML = resultMbti.jobs[i]
+})
 
-boxEls.forEach((el, i) => {
-  el.innerHTML = 'box {i + 1}';
-});
-
-jobsEls.forEach((el, i) =>
-  el.innerHTML = `jobs ${i + 1}` <Image>
-);
+lectureEl.href = resultMbti.lectureUrl
+lectureImgEl.src = resultMbti.lectureImg
